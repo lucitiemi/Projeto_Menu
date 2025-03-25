39d1d327402ad58cf74fa872ede7c55b917f5707
Luciana <lt.shimoyama@gmail.com>
2025-03-24 21:24:43 -0300

    closes #4 - Implementar componente quantidade

---
+++ js/javascript.js
@@ -0,0 +1,27 @@
+var prods = [
+    { id: 1, name: "Bife com batata", price: 30.0 },
+    { id: 2, name: "Coxa de frango crocante", price: 25.0 },
+    { id: 3, name: "Carne de panela", price: 22.0 },
+    { id: 4, name: "Farofa", price: 10.0 },
+    { id: 5, name: "Salada", price: 8.0 },
+    { id: 6, name: "Torresmo", price: 12.0 },
+];
+
+function calc(){
+    var quantities = document.getElementsByName("quantity");
+    var output     = document.getElementById("output");
+    var total      = 0;
+    
+    output.innerHTML = "";
+
+    for (var input of quantities) {
+        var id = input.id;
+
+        output.innerHTML += `Produto: ${prods[id-1].name}  - Preço: ${prods[id-1].price} - Quantidade: ${input.value} </br>`;
+        total            += prods[id-1].price * parseFloat(input.value);
+    }
+
+    output.innerHTML += `<h2>Total: ${total}</h2>`;
+
+}
+
